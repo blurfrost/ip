a3d2bedd972ebe32266e9441965df712415cb8a9
blurfrost <ksyc07@gmail.com>
2025-08-29 09:38:24 +0800

    Edit Todo.java to conform to coding standard

--- src/main/java/performative/tasks/Todo.java
+++ src/main/java/performative/tasks/Todo.java
@@ -1,13 +1,14 @@
 package performative.tasks;
 
 public class Todo extends Task {
-    public Todo (String description) {
+    public Todo(String description) {
         super(description);
     }
 
     @Override
     public String toSaveFormat() {
-        return "performative.tasks.Todo; " + (super.getStatus() ? "Complete" : "Incomplete") + "; " + super.getDescription();
+        return "performative.tasks.Todo; " + (super.getStatus() ? "Complete" : "Incomplete")
+                + "; " + super.getDescription();
     }
     @Override
     public String toString() {
